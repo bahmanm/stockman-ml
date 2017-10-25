ae420d8709ff16973261e8fc9f6098ebcb2fd7db
Bahman Movaqar <bahman@bahmanm.com>
2017-10-25 05:18:09 +0200

    add `product_of_string`

--- src/stockman.ml
+++ src/stockman.ml
@@ -5,13 +5,13 @@ module Products = struct
   class product = object (self)
     val mutable name = ""
     val mutable qty = 0
-                    
+
     method get_name = name
     method set_name _name = name <- _name
-                          
+
     method get_qty = qty
     method set_qty _qty = qty <- _qty
-  end;;
+  end
 
   module Repo = struct    
     type db_t = product list
@@ -19,7 +19,31 @@ module Products = struct
     let size db = BatList.length db
     let is_empty db = BatList.is_empty db
     let empty = BatList.make 0 new product
-  end;;
-    
-end;;
-  
+  end
+
+  module Etl = struct
+    let n_fields = 2
+      
+    let product_of_string delimiter str =
+      let is_valid_name s = String.length s > 0 in
+      let is_valid_qty s = Str.string_match (Str.regexp "^[0-9]+$") s 0 in
+      let get_fields s = s |> String.split_on_char delimiter
+                         |> BatList.map String.trim in
+      let is_valid_fields fields = BatList.length fields = n_fields &&
+                                   BatList.at fields 0 |> is_valid_name &&
+                                   BatList.at fields 1 |> is_valid_qty in
+      if String.length str = 0 then
+        Error("Empty row")
+      else
+        let fields = get_fields str in
+        if is_valid_fields fields then
+          let p = new product in
+          BatList.at fields 0 |> p#set_name;
+          BatList.at fields 1 |> int_of_string |> p#set_qty;
+          Ok(p)
+        else
+          Error("Invalid row: " ^ str)
+  end  
+
+end
+
--- test/test_stockman.ml
+++ test/test_stockman.ml
@@ -2,6 +2,7 @@ open OUnit2;;
 
 module R = Stockman.Products.Repo;;
 module T = Stockman.Products;;
+module E = Stockman.Products.Etl;;
   
 let test_product_db_add ctx =
   let db = R.empty in
@@ -12,12 +13,42 @@ let test_product_db_add ctx =
   assert_equal 2 (R.size db);;
       
 let test_product_db_empty ctx =
-  assert_equal 1 (R.size R.empty);;
+  assert_equal 0 (R.size R.empty);;
 
+let test_etl_product_of_string_valid_input ctx =
+  let () = match E.product_of_string ',' "p1,10" with
+    | Ok p -> assert_equal "p1" p#get_name;
+      assert_equal 10 p#get_qty;
+    | _ -> assert_equal 0 1 in
+  let () = match E.product_of_string ';' "p1 ;10 " with
+    | Ok p -> assert_equal "p1" p#get_name;
+      assert_equal 10 p#get_qty;
+    | _ -> assert_equal 0 1 in
+  match E.product_of_string '-' "   p1 -  10 " with
+  | Ok p -> assert_equal "p1" p#get_name;
+      assert_equal 10 p#get_qty
+  | _ -> assert_equal 0 1;;
+
+let test_etl_product_of_string_invalid_input ctx =
+  let () = match E.product_of_string ',' ",10" with
+    | Error msg -> assert_equal msg "Invalid row: ,10"
+    | _ -> assert_equal 0 1 in
+  let () = match E.product_of_string ';' "p1, " with
+    | Error msg -> assert_equal msg "Invalid row: p1, "
+    | _ -> assert_equal 0 1 in
+  let () = match E.product_of_string ',' "p1" with
+    | Error msg -> assert_equal msg "Invalid row: p1"
+    | _ -> assert_equal 0 1 in
+  match E.product_of_string '-' "" with
+  | Error msg -> assert_equal msg "Empty row"
+  | _ -> assert_equal 0 1;;
+                
 let suite =
   "suite">:::
-    ["test_product_db_add">:: test_product_db_add;
-     "test_product_db_empty">:: test_product_db_empty];;
+  ["test_product_db_add">:: test_product_db_add;
+   "test_product_db_empty">:: test_product_db_empty;
+   "test_etl_product_of_string_valid_input">:: test_etl_product_of_string_valid_input;
+   "test_etl_product_of_string_invalid_input">:: test_etl_product_of_string_invalid_input];;
 
 let () =
   run_test_tt_main suite
