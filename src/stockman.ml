b5b21a0ef747fc1342d31ae1dbb0b9bae18f1c89
Bahman Movaqar <bahman@bahmanm.com>
2017-10-29 15:31:11 +0100

    add the glue - it's a fully functional WP1!
    still not happy:
    * need to clean up `stockman.ml` --very cluttered
    * probably get rid of `Products.Db`
    * remove duplicate check for validity of cli options (in `Cmd` and `main`)

--- src/stockman.ml
+++ src/stockman.ml
@@ -133,4 +133,15 @@ module Cmd = struct
     Arg.parse_argv argv specs anon_f "Stockman";
     validate_opts cmd_opts
 end
-
+  
+let () =
+  match Cmd.parse Sys.argv with
+  | Ok { Cmd.file_path = Some file_path; field_delim = Some field_delim } -> 
+    DsvLoader.db_of_file file_path "#" field_delim 1
+    |> BatList.sort (fun p1 p2 -> - compare (p1.Products.qty) (p2.Products.qty))
+    |> TableFormatter.format
+    |> print_endline
+  | Ok { Cmd.file_path = _; field_delim = _ } ->
+    print_endline "Invalid command line args"
+  | Bad s ->
+    print_endline s;;
