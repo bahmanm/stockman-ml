c2998856e63cfd7a7465f726d2196ce2d5e203be
Bahman Movaqar <bahman@bahmanm.com>
2017-11-02 22:05:39 +0100

    add command line options
    * `-i`: how many header lines to ignore
    * `-c`: comment marker

--- src/cmdLine.ml
+++ src/cmdLine.ml
@@ -6,6 +6,8 @@ module CmdLine : sig
   type cmd_opts_t = {
     file_path : string;
     field_delim : char;
+    n_header : int;
+    comment_marker : string;
   }
   (** Stockman command line options *)    
 
@@ -17,11 +19,15 @@ end = struct
   type cmd_opts_t = {
     file_path : string;
     field_delim : char;
+    n_header : int;
+    comment_marker : string;
   }
 
   type cmd_opts_wip_t = {
     mutable wip_file_path : string option;
     mutable wip_field_delim : char option;
+    mutable wip_n_header : int option;
+    mutable wip_comment_marker : string option;
   }
   (* command line options - during parsing and validation *)
 
@@ -32,22 +38,46 @@ end = struct
     ("-d",
      Arg.String (fun arg ->
          opts.wip_field_delim <- (BatString.enum arg |> BatEnum.get)),
-     "field delimiter character")
+     "field delimiter character");
+    ("-i",
+     Arg.Int (fun arg ->
+         opts.wip_n_header <- Some arg),
+     "field delimiter character");
+    ("-c",
+     Arg.String (fun arg ->
+         opts.wip_comment_marker <- Some arg),
+     "comment marker")
   ]
 
   let validate_opts cmd_opts =
-    match cmd_opts.wip_file_path with
-    | Some file_path ->
-      (match cmd_opts.wip_field_delim with
-       | Some field_delim -> Ok {
-           file_path = file_path;
-           field_delim = field_delim
-         }
-       | _ -> Bad "field delimiter cannot be empty.")
-    | _ -> Bad "file path cannot be empty."
+    match cmd_opts with
+    | { wip_file_path = None; _ } ->
+      Bad "file path cannot be empty."
+    | { wip_field_delim = None; _ } ->
+      Bad "field delimiter cannot be empty."
+    | { wip_comment_marker = None; _ } ->
+      Bad "comment marker cannot be empty."
+    | { wip_n_header = None; _ } ->
+      Bad "number of header lines cannot be empty."
+    | { wip_n_header = Some i; _ } when i < 0 ->
+      Bad "number of header lines cannot be < 0."
+    | { wip_file_path = Some file_path;
+        wip_field_delim = Some field_delim;
+        wip_n_header = Some n_header;
+        wip_comment_marker = Some comment_marker
+      } ->
+      Ok {
+        file_path = file_path; field_delim = field_delim;
+        n_header = n_header; comment_marker = comment_marker
+      }
   
   let parse argv =
-    let cmd_opts = { wip_file_path = None; wip_field_delim = Some ',' } in
+    let cmd_opts = {
+      wip_file_path = None;
+      wip_field_delim = Some ',';
+      wip_comment_marker = Some "#";
+      wip_n_header = Some 1
+    } in
     let anon_f s = () in
     Arg.current := 0;
     Arg.parse_argv argv (specs cmd_opts) anon_f "Stockman";
--- src/stockman.ml
+++ src/stockman.ml
@@ -5,8 +5,13 @@ module C = CmdLine.CmdLine
 
 let () =
   match C.parse Sys.argv with
-  | Ok { C.file_path = file_path; field_delim = field_delim } -> 
-    DsvLoader.db_of_file file_path "#" field_delim 1
+  | Ok {
+      C.file_path = file_path;
+      field_delim = field_delim;
+      n_header = n_header;
+      comment_marker = comment_marker
+    } -> 
+    DsvLoader.db_of_file file_path comment_marker field_delim n_header
     |> Product.Db.sort (fun p1 p2 -> - compare (p1.Product.qty) (p2.Product.qty))
     |> Product.Db.to_list
     |> TableFormatter.format
