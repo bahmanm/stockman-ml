04666f5fabfa29c4c508267d3a24078360c3da1f
Bahman Movaqar <bahman@bahmanm.com>
2017-11-09 11:03:57 +0100

    WIP invoice service

---
+++ src/service/stkService_Invoice.ml
@@ -0,0 +1,10 @@
+(* Author: Bahman Movaqar <Bahman@BahmanM.com> *)
+(** Purchase Invoice service. *)
+open Batteries
+
+module InvoiceDb = StkDomainDb.InvoiceDb
+module Invoice = StkDomain.Product
+
+(** [save inv db] saves [inv] into [db]. In case an invoice already exists
+    with the same `id`, it will combine the lines. *)
+let save inv db = db
