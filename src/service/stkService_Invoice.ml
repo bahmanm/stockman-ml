69f2e1446ab231bf3ad8eae55cea2d257019b729
Bahman Movaqar <bahman@bahmanm.com>
2017-12-09 09:25:58 +0100

    implement `save` operation

--- src/service/stkService_Invoice.ml
+++ src/service/stkService_Invoice.ml
@@ -2,10 +2,39 @@
 (** Purchase Invoice service. *)
 open Batteries
 
-module InvoiceDb = StkDomainDb.InvoiceDb
-module Invoice = StkDomain.Product
+module IDb = StkDomainDb.InvoiceDb
+module I = StkDomain.Invoice
+
+module Utils = struct
+  let combine_lines inv1 inv2 =
+    let lines1 = inv1.I.lines in
+    let lines2 = inv2.I.lines in
+    let lines_diff =
+      List.filter
+        (fun l1 ->
+           List.exists
+             (fun l2 -> l2.I.line_no = l1.I.line_no)
+             lines2
+           |> not)
+        lines1 in
+    List.append lines2 lines_diff
+
+  let calc_amt lines =
+    List.fold_left
+      (fun amt line -> amt +. (line.I.qty |> float_of_int) *. line.I.price)
+      0.
+      lines
+end
 
 (** [save inv db] saves [inv] into [db]. In case an invoice already exists
     with the same `id`, it will combine the lines. *)
 let save inv db =
-  InvoiceDb.save inv db
+  match (db |> IDb.get inv.I.doc_no) with
+  | None -> IDb.save inv db
+  | Some inv_db -> begin
+      let lines = Utils.combine_lines inv inv_db in
+      let amt = Utils.calc_amt lines in
+      IDb.save
+        { inv with I.amt = amt; I.lines = lines }
+        db
+    end
