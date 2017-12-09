1965bca5cc14571edfe792e1bfc5ea3c6ac0dd1c
Bahman Movaqar <bahman@bahmanm.com>
2017-12-09 09:25:30 +0100

    run invoice service `save` test suite

--- test/service/test_service.ml
+++ test/service/test_service.ml
@@ -2,7 +2,7 @@
 open OUnit2
 
 let () =
-  print_endline "█ test_service.suite_inventory";
+  print_endline "█ test_service.test_inventory";
   run_test_tt_main Test_inventory.suite_inventory;
-  print_endline "█ test_service.suite_invoice";
-  run_test_tt_main Test_invoice.suite_invoice;
+  print_endline "█ test_service.test_invoice.save";
+  run_test_tt_main Test_invoice.suite_save;
