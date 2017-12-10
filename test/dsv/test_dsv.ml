ad4fe9e6e333d29b4259be8b8b309cee3ccf0b7c
Bahman Movaqar <bahman@bahmanm.com>
2017-12-10 19:54:52 +0100

    add invoice tests to dsv test runner - all failing

--- test/dsv/test_dsv.ml
+++ test/dsv/test_dsv.ml
@@ -6,4 +6,10 @@ let print_name s =
 
 let () =
   print_name Test_product.Product_of_string.name;
-  run_test_tt_main Test_product.Product_of_string.suite
+  run_test_tt_main Test_product.Product_of_string.suite;
+
+  print_name Test_invoice.Invoice_of_string.name;
+  run_test_tt_main Test_invoice.Invoice_of_string.suite;
+
+  print_name Test_invoice.Load_file.name;
+  run_test_tt_main Test_invoice.Load_file.suite
--- test/dsv/test_invoice.ml
+++ test/dsv/test_invoice.ml
@@ -5,10 +5,42 @@ open Batteries
 module I = StkDomain.Invoice
 module Dsv = StkDsv.Invoice
 
+let name = "test_invoice."
+
 (**********************************************************)
 module Invoice_of_string = struct
-  let test_empty_string =
+  let name = name ^ "invoice_of_string"
+
+  (************************)
+  let test_empty_string ctx =
     match (Dsv.invoice_of_string ',' "") with
     | Ok _ -> assert false
     | Bad s -> assert false
+
+  (************************)
+  let suite =
+    name >:::
+    ["empty string"
+     >:: test_empty_string]
+end
+
+(**********************************************************)
+module Load_file = struct
+  let name = name ^ "load_file"
+
+  (************************)
+  let test_valid_input ctx =
+    assert false
+
+  (************************)
+  let test_invalid_input ctx =
+    assert false
+
+  (************************)
+  let suite =
+    name >:::
+    ["valid input"
+     >:: test_valid_input;
+     "invalid_input"
+     >:: test_invalid_input]
 end
